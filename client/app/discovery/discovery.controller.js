ee268e571d499b759267135caeeab9dac0e2032a
Brian Kustra <bkustrakb@gmail.com>
2015-12-29 17:18:00 -0600

    Git stash pop'd to this branch
    Switched artist and admin, thanks Ryan. Switching branch
    
    Routes are working, everything except picture upload seems to be working

--- client/app/discovery/discovery.controller.js
+++ client/app/discovery/discovery.controller.js
@@ -1,23 +1,30 @@
 'use strict';
 
 angular.module('culturalystApp')
-.controller('DiscoveryCtrl', function($scope, MediaList, $http){
+.controller('DiscoveryCtrl', function($scope, MediaList, $http, $location){
   $scope.mediaList = MediaList.getMediaList();
-  console.log('hi')
-  console.log($scope.artists)
+  // console.log($scope.artists)
 
+  $scope.getArtists = function() {
+    $http.get('/api/users/discovery/' + $scope.selectedMedium + '/' + $scope.selectedSubmedium).then(function(response) {
+      $scope.artists = response.data;
+      // console.log(response.data, "GET ARTISTS");
+    })
+  }
 
-    $scope.getArtists = function() {
-      $http.get('/api/users/discovery/' + $scope.selectedMedium + '/' + $scope.selectedSubmedium).then(function(response) {
-        $scope.artists = response.data;
-        console.log(response.data);
-      })
-    }
+  $scope.loadSubMediums = function(medium){
+    // console.log(medium.name)
+    $scope.selectedMedium = medium.name;
+    $scope.submedia = medium.submedia;
+  };
 
-    $scope.loadSubMediums = function(medium){
-      console.log(medium.name)
-      $scope.selectedMedium = medium.name;
-      $scope.submedia = medium.submedia;
-    };
+  $scope.populatePage = function() {
+    $scope.pageMedium = $location.path().split('/').pop();
+    $http.get('/api/users/discovery/' + $scope.pageMedium + '/undefined' ).then(function(response) {
+      $scope.artists = response.data;
+      console.log($scope.pageMedium, 'populatePage');
+    })
+  }
+  $scope.populatePage()
 
 });
